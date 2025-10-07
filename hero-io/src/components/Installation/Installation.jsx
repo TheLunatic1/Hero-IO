bbbc154bc59a93d603a42baa494349df4151a024
Lunatic1 <112961007+TheLunatic1@users.noreply.github.com>
2025-10-08 01:26:41 +0600

    Update Installation.jsx

--- hero-io/src/components/Installation/Installation.jsx
+++ hero-io/src/components/Installation/Installation.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Installation = () => {
   return (
     <div>
-      <h1>Installation Guide</h1>
+      <h1>Installation</h1>
       // Add content
     </div>
   );
